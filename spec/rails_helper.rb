b81c22423f6ece8013780285bf208898349ec01a
Eetu Mattila <eetu_mattila@hotmail.com>
2017-01-14 23:26:08 +0200

    .

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(Array[
+    Coveralls::SimpleCov::Formatter,
+    SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start 'rails'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
