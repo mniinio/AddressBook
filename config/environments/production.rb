a1fc7cd81ba01ae9252f5f728c8a886f299fb0da
Eetu Mattila <eetu_mattila@hotmail.com>
2017-01-15 18:07:15 +0200

    Sengrid test 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
       address:              'smtp.sendgrid.net',
       port:                 587,
-      domain:               'the-addressbook.herokuapp.com',
+      domain:               'heroku.com',
       user_name:            ENV["SENDGRID_USERNAME"],
       password:             ENV["SENDGRID_PASSWORD"],
       authentication:       'plain',
