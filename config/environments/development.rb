d3b2c6d295f3e08a6551f4aed2b9e88f38682f2b
Eetu Mattila <eetu_mattila@hotmail.com>
2017-01-15 17:52:29 +0200

    Sengrid test 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,16 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'the-addressbook.herokuapp.com',
+      user_name:            ENV["GMAIL_USERNAME"],
+      password:             ENV["GMAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'the-addressbook.herokuapp.com/' }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 587,
+      domain:               'the-addressbook.herokuapp.com',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 end
