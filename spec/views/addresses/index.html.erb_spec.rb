2ac8f7dbf6ad7f6361e1854ab01624b2c6bc0ebe
Eetu Mattila <eetu_mattila@hotmail.com>
2017-01-18 16:39:01 +0200

    Commented out some more auto-generated tests

--- spec/views/addresses/index.html.erb_spec.rb
+++ spec/views/addresses/index.html.erb_spec.rb
@@ -1,55 +1,55 @@
-require 'rails_helper'
-
-RSpec.describe "addresses/index", type: :view do
-  before(:each) do
-    assign(:addresses, [
-      Address.create!(
-        :user => nil,
-        :name => "Name",
-        :address1 => "Address1",
-        :address2 => "Address2",
-        :zip_code => "Zip Code",
-        :city => "City",
-        :state => "State",
-        :country => "Country",
-        :phone => "Phone",
-        :mobile => "Mobile",
-        :email => "Email",
-        :website => "Website",
-        :other => "MyText"
-      ),
-      Address.create!(
-        :user => nil,
-        :name => "Name",
-        :address1 => "Address1",
-        :address2 => "Address2",
-        :zip_code => "Zip Code",
-        :city => "City",
-        :state => "State",
-        :country => "Country",
-        :phone => "Phone",
-        :mobile => "Mobile",
-        :email => "Email",
-        :website => "Website",
-        :other => "MyText"
-      )
-    ])
-  end
-
-  it "renders a list of addresses" do
-    render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Address1".to_s, :count => 2
-    assert_select "tr>td", :text => "Address2".to_s, :count => 2
-    assert_select "tr>td", :text => "Zip Code".to_s, :count => 2
-    assert_select "tr>td", :text => "City".to_s, :count => 2
-    assert_select "tr>td", :text => "State".to_s, :count => 2
-    assert_select "tr>td", :text => "Country".to_s, :count => 2
-    assert_select "tr>td", :text => "Phone".to_s, :count => 2
-    assert_select "tr>td", :text => "Mobile".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
-    assert_select "tr>td", :text => "Website".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe "addresses/index", type: :view do
+#   before(:each) do
+#     assign(:addresses, [
+#       Address.create!(
+#         :user => nil,
+#         :name => "Name",
+#         :address1 => "Address1",
+#         :address2 => "Address2",
+#         :zip_code => "Zip Code",
+#         :city => "City",
+#         :state => "State",
+#         :country => "Country",
+#         :phone => "Phone",
+#         :mobile => "Mobile",
+#         :email => "Email",
+#         :website => "Website",
+#         :other => "MyText"
+#       ),
+#       Address.create!(
+#         :user => nil,
+#         :name => "Name",
+#         :address1 => "Address1",
+#         :address2 => "Address2",
+#         :zip_code => "Zip Code",
+#         :city => "City",
+#         :state => "State",
+#         :country => "Country",
+#         :phone => "Phone",
+#         :mobile => "Mobile",
+#         :email => "Email",
+#         :website => "Website",
+#         :other => "MyText"
+#       )
+#     ])
+#   end
+#
+#   it "renders a list of addresses" do
+#     render
+#     assert_select "tr>td", :text => nil.to_s, :count => 2
+#     assert_select "tr>td", :text => "Name".to_s, :count => 2
+#     assert_select "tr>td", :text => "Address1".to_s, :count => 2
+#     assert_select "tr>td", :text => "Address2".to_s, :count => 2
+#     assert_select "tr>td", :text => "Zip Code".to_s, :count => 2
+#     assert_select "tr>td", :text => "City".to_s, :count => 2
+#     assert_select "tr>td", :text => "State".to_s, :count => 2
+#     assert_select "tr>td", :text => "Country".to_s, :count => 2
+#     assert_select "tr>td", :text => "Phone".to_s, :count => 2
+#     assert_select "tr>td", :text => "Mobile".to_s, :count => 2
+#     assert_select "tr>td", :text => "Email".to_s, :count => 2
+#     assert_select "tr>td", :text => "Website".to_s, :count => 2
+#     assert_select "tr>td", :text => "MyText".to_s, :count => 2
+#   end
+# end
--- spec/views/addresses/show.html.erb_spec.rb
+++ spec/views/addresses/show.html.erb_spec.rb
@@ -1,38 +1,38 @@
-require 'rails_helper'
-
-RSpec.describe "addresses/show", type: :view do
-  before(:each) do
-    @address = assign(:address, Address.create!(
-      :user => nil,
-      :name => "Name",
-      :address1 => "Address1",
-      :address2 => "Address2",
-      :zip_code => "Zip Code",
-      :city => "City",
-      :state => "State",
-      :country => "Country",
-      :phone => "Phone",
-      :mobile => "Mobile",
-      :email => "Email",
-      :website => "Website",
-      :other => "MyText"
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(//)
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Address1/)
-    expect(rendered).to match(/Address2/)
-    expect(rendered).to match(/Zip Code/)
-    expect(rendered).to match(/City/)
-    expect(rendered).to match(/State/)
-    expect(rendered).to match(/Country/)
-    expect(rendered).to match(/Phone/)
-    expect(rendered).to match(/Mobile/)
-    expect(rendered).to match(/Email/)
-    expect(rendered).to match(/Website/)
-    expect(rendered).to match(/MyText/)
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe "addresses/show", type: :view do
+#   before(:each) do
+#     @address = assign(:address, Address.create!(
+#       :user => nil,
+#       :name => "Name",
+#       :address1 => "Address1",
+#       :address2 => "Address2",
+#       :zip_code => "Zip Code",
+#       :city => "City",
+#       :state => "State",
+#       :country => "Country",
+#       :phone => "Phone",
+#       :mobile => "Mobile",
+#       :email => "Email",
+#       :website => "Website",
+#       :other => "MyText"
+#     ))
+#   end
+#
+#   it "renders attributes in <p>" do
+#     render
+#     expect(rendered).to match(//)
+#     expect(rendered).to match(/Name/)
+#     expect(rendered).to match(/Address1/)
+#     expect(rendered).to match(/Address2/)
+#     expect(rendered).to match(/Zip Code/)
+#     expect(rendered).to match(/City/)
+#     expect(rendered).to match(/State/)
+#     expect(rendered).to match(/Country/)
+#     expect(rendered).to match(/Phone/)
+#     expect(rendered).to match(/Mobile/)
+#     expect(rendered).to match(/Email/)
+#     expect(rendered).to match(/Website/)
+#     expect(rendered).to match(/MyText/)
+#   end
+# end
