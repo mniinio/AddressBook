dce6b830533f839f7dd0a8f7d71ff58e4994d314
Eetu Mattila <eetu_mattila@hotmail.com>
2017-01-24 04:16:29 +0200

    Minor addition to validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,6 +5,7 @@ class Address < ApplicationRecord
   # Addresses are for the use of User himself so no point of stopping him trying to enter 'batman' as phone number
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
+  validates :address1, presence: true, if: :address2?
   validates :name, :zip_code, :country, :state, :phone, :mobile, :email, :website, length: { maximum: 50 }
   validates :address1, :address2, length: { maximum: 200 }
   validates :other, length: { maximum: 2000 }
