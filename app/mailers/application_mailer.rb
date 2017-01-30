9f4ddf4ffd4b93b6414a34869822d8d0913c9c0d
Marjukka Niinioja <marjukka.niinioja@kesko.fi>
2017-01-30 23:48:49 +0200

    Mailgun integration

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+class ApplicationMailer < ActionMailer::Base.smtp_settings = {
+  :address        => ENV['smtp.mailgun.org'],
+  :user_name      => ENV['postmaster@app839c9f84ade24577a85d70365434e3c9.mailgun.org'],
+  :password       => ENV['09b9c52ee6b9ef3aeaf7e0e2d0307ef7'],
+  :domain         => 'https://enigmatic-bayou-52069.herokuapp.com/',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
 end
