ee612941da618998050355578be58931f1db6704
Marjukka Niinioja <marjukka.niinioja@gmail.com>
2017-02-05 20:26:33 +0200

    superuser to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117133706) do
+ActiveRecord::Schema.define(version: 20170205182233) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20170117133706) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "superuser"
+    t.         "default"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
